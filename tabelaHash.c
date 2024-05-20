55c3c7750ab17a8751a447dd3e45cf89b518b789
Guilherme R. Beira <149908299+GuiRBeira@users.noreply.github.com>
2024-05-20 10:22:06 -0300

    Add files via upload

--- tabelaHash.c
+++ tabelaHash.c
@@ -27,11 +27,10 @@ void inicializaTabelaHash(TabelaHash *c){
 int funcaoHash(TabelaHash *c, int id){
     //metodo da multiplicacao
     int posicao = (int)(SIZE*((id*A)-(int)(id*A)));
-    for(posicao; posicao < SIZE; (++posicao)%SIZE){
-        if(c->cardapio[posicao].id == -1){
-            return posicao;
-        }
-    }
+    do{
+        if(c->cardapio[posicao].id == -1) return posicao;
+        ++posicao; posicao%=SIZE;
+    }while(true);
 }
 
 bool inserirElemento(TabelaHash *c, Produto e){
@@ -40,13 +39,30 @@ bool inserirElemento(TabelaHash *c, Produto e){
         return false;
     } else {
         int posicao = funcaoHash(c, e.id); 
-        c->q_elements++;       
+        c->cardapio[posicao] = e;
+        c->q_elements++;
+        return true;
     }
-    
 }
+
+void imprimeTabela(TabelaHash *c){
+    printf("Cardapio pizzaria\n");
+    for(int i = 0; i < SIZE; i++){
+        if(c->cardapio[i].id != -1){
+            printf("Posicao: %d\nID: %d\n", i, c->cardapio[i].id);
+            printf("Sabor: %s\n", c->cardapio[i].sabor);
+            printf("Preço: %.2f\n",c->cardapio[i].valor);
+            printf("------------------------------\n");
+        }
+    }
+    printf("Fim\n");
+}
+
+int buscaElemento(TabelaHash *c, int id);
 
 int main(){
     TabelaHash tabela;
+
     inicializaTabelaHash(&tabela);
     inserirElemento(&tabela, (Produto){86031, "Bacon", 23.00});
     inserirElemento(&tabela, (Produto){12011, "Marguerita", 23.00});
@@ -54,7 +70,30 @@ int main(){
     inserirElemento(&tabela, (Produto){22132, "Frango", 30.00});
     inserirElemento(&tabela, (Produto){18138, "Camarao", 48.00});
     inserirElemento(&tabela, (Produto){13133, "Calabresa", 25.00});
-    inserirElemento(&tabela, (Produto){03121, "Palmito", 35.00});
+    inserirElemento(&tabela, (Produto){13121, "Palmito", 35.00});
     inserirElemento(&tabela, (Produto){11210, "Picanha", 45.00});
+    imprimeTabela(&tabela);
+    
+    int pos = buscaElemento(&tabela, 13121);
+    if(pos >= 0){
+        printf("Posicao: %d\nID: %d\n", pos, tabela.cardapio[pos].id);
+        printf("Sabor: %s\n", tabela.cardapio[pos].sabor);
+        printf("Preço: %.2f\n",tabela.cardapio[pos].valor);
+        printf("------------------------------\n");
+    } else {
+        printf("Elemento não encontrado..\n");
+    }
     return EXIT_SUCCESS;
+}
+
+int buscaElemento(TabelaHash *c, int id){
+    int posicao = (int)(SIZE*((id*A)-(int)(id*A)));
+    if(c->cardapio[posicao].id == id) return posicao;
+    int i = 0;
+    do{
+        ++posicao; posicao%=SIZE;
+        if(c->cardapio[posicao].id == id) return posicao;
+        else if (c->cardapio[posicao].id == -1) return -1;
+        i++;
+    }while (i <= SIZE);
 }
